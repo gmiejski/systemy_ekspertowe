aca92e3c0d2887842dfcec00660d98b9d2bb53b8
marek1840 <marek1840@gmail.com>
2015-12-02 21:04:28 +0100

    reading theory from public resource file

--- webapp/app/controllers/Application.scala
+++ webapp/app/controllers/Application.scala
@@ -1,10 +1,15 @@
 package controllers
 
+import java.io.InputStream
+
 import alice.tuprolog.{Prolog, SolveInfo, Theory}
 import com.google.gson.Gson
-import play.api.Logger
+import play.api.Play.current
 import play.api.libs.json.JsValue
 import play.api.mvc._
+import play.api.{Logger, Play}
+
+import scala.io.Source
 
 class Application extends Controller {
   val prolog = new Prolog()
@@ -38,10 +43,12 @@ class Application extends Controller {
 
     maybeTheory.map { t =>
       prolog.clearTheory()
-      prolog.addTheory(Application.mainTheory)
+      val mainTheory = Application.mainTheory
+        .get
+      prolog.addTheory(mainTheory)
       prolog.addTheory(t)
 
-      val info: SolveInfo = prolog.solve("jezyk(X).")
+      val info: SolveInfo = prolog.solve("language(X).")
       Logger.info("success: " + info.isSuccess)
       Logger.info("hasAlts: " + info.hasOpenAlternatives)
       Logger.info(info + "")
@@ -53,188 +60,9 @@ class Application extends Controller {
 }
 
 object Application {
-  private[this] val mainTheorySource =
-    """
-      |jezyk(ruby) :- is(simple),
-      |               typed(dynamically),
-      |               true(has, duckTyping),
-      |               true(has, lambdas),
-      |               true(has, typeInference),
-      |               false(has, patternMatching),
-      |               false(has, lazyEvaluation),
-      |               false(has, actors),
-      |               paradigm(object-oriented),
-      |               paradigm(metaprogramming),
-      |               (
-      |                 paradigm(imperative);
-      |                 paradigm(functional)
-      |               ),
-      |               interpreted.
-      |
-      |jezyk(c) :-      is(simple),
-      |                 typed(statically),
-      |                 false(has, patternMatching),
-      |                 false(has, lambdas),
-      |                 false(has, typeInference),
-      |                 false(has, duckTyping),
-      |                 false(has, lazyEvaluation),
-      |                 paradigm(imperative),
-      |                 paradigm(metaprogramming),
-      |                 usable_from_ide(visualStudio),
-      |                 run_directly.
-      |
-      |jezyk(scala) :- is(complicated),
-      |                typed(statically),
-      |                true(has, patternMatching),
-      |                true(has, lambdas),
-      |                true(has, typeInference),
-      |                false(has, duckTyping),
-      |                paradigm(object-oriented),
-      |                paradigm(lazy),
-      |                paradigm(concurrent),
-      |                paradigm(metaprogramming),
-      |                (
-      |                   paradigm(imperative);
-      |                   paradigm(functional)
-      |                ),
-      |                (
-      |                   usable_from_ide(intellij);
-      |                   usable_from_ide(eclipse)
-      |                ),
-      |                write_once_ruin_everywhere.
-      |
-      |jezyk(csharp) :- is(simple),
-      |                 typed(statically),
-      |                 false(has, patternMatching),
-      |                 true(has, lambdas),
-      |                 true(has, typeInference),
-      |                 false(has, duckTyping),
-      |                 false(has, actors),
-      |                 false(has, lazyEvaluation),
-      |                 false(has, macros),
-      |                 paradigm(object-oriented),
-      |                 paradigm(imperative),
-      |                 usable_from_ide(visualStudio),
-      |                 write_once_ruin_everywhere.
-      |
-      |jezyk(java) :-  is(simple),
-      |                typed(statically),
-      |                false(has, patternMatching),
-      |                true(has, lambdas),
-      |                false(has, typeInference),
-      |                false(has, duckTyping),
-      |                false(has, actors),
-      |                false(has, lazyEvaluation),
-      |                false(has, macros),
-      |                paradigm(object-oriented),
-      |                paradigm(imperative),
-      |                (
-      |                   usable_from_ide(intellij);
-      |                   usable_from_ide(eclipse)
-      |                ),
-      |                write_once_ruin_everywhere.
-      |
-      |jezyk(haskell)  :-  is(complicated),
-      |                    typed(statically),
-      |                    true(has, patternMatching),
-      |                    true(has, lambdas),
-      |                    true(has, typeInference),
-      |                    true(has, duckTyping),
-      |                    false(has, actors),
-      |                    false(has, macros),
-      |                    paradigm(functional),
-      |                    paradigm(lazy),
-      |                    usable_from_ide(intellij),
-      |                    run_directly.
-      |
-      |jezyk(python) :-    is(simple),
-      |                    typed(dynamically),
-      |                    false(has, patternMatching),
-      |                    false(has, typeInference),
-      |                    true(has, lambdas),
-      |                    true(has, duckTyping),
-      |                    false(has, actors),
-      |                    false(has, lazyEvaluation),
-      |                    false(has, macros),
-      |                    paradigm(object-oriented),
-      |                    (
-      |                       paradigm(imperative);
-      |                       paradigm(functional)
-      |                    ),
-      |                    usable_from_ide(intellij),
-      |                    interpreted.
-      |
-      |jezyk(lisp) :-  is(complicated),
-      |                typed(dynamically),
-      |                true(has, lambdas),
-      |                true(has, patternMatching),
-      |                false(has, typeInheritance),
-      |                false(has, lazyEvaluation),
-      |                false(has, actors),
-      |                paradigm(object-oriented),
-      |                paradigm(metaprogramming),
-      |                paradigm(functional),
-      |                write_once_ruin_everywhere.
-      |
-      |jezyk(erlang) :- is(complicated),
-      |                 typed(dynamically),
-      |                 true(has, patternMatching),
-      |                 true(has, lambdas),
-      |                 true(has, typeInference),
-      |                 true(has, duckTyping),
-      |                 false(has, lazyEvaluation),
-      |                 false(has, macros),
-      |                 paradigm(functional),
-      |                 paradigm(object-oriented),
-      |                 paradigm(concurrent),
-      |                 usable_from_ide(intellij),
-      |                 write_once_ruin_everywhere.
-      |
-      |
-      |
-      |is(simple) :- true(has, simpleSyntax), true(has, clearDocumentation).
-      |is(complicated) :-  false(has, simpleSyntax).
-      |is(complicated) :-  false(has, clearDocumentation).
-      |
-      |typed(statically) :-  true(has, types_resolved_during_compilation).
-      |typed(dynamically) :- false(has, types_resolved_during_compilation).
-      |
-      |usable_from_ide(X) :- true(mozna_uzywac_z, X).
-      |
-      |write_once_ruin_everywhere :- true(has, virtualMachine).
-      |
-      |interpreted :- true(has, interpreter).
-      |
-      |run_directly :- false(has, virtualMachine),
-      |                false(has, interpreter).
-      |
-      |paradigm(object-oriented) :- true(has, typeInheritance),
-      |                             true(has, encapsulation).
-      |
-      |paradigm(imperative) :- true(has, mutation),
-      |                        true(has, sideEffects).
-      |
-      |paradigm(functional) :- false(has, sideEffects),
-      |                        false(has, mutation).
-      |
-      |paradigm(concurrent) :- true(has, actors).
-      |
-      |paradigm(metaprogramming) :- true(has, macros).
-      |
-      |paradigm(lazy) :- true(has, lazyEvaluation).
-      |
-      |true(X,Y) :- (
-      |                     yes(X, Y);
-      |                     neutral(X, Y)
-      |                  ), !.
-      |
-      |false(X,Y) :- (
-      |                     no(X,Y);
-      |                     neutral(X, Y)
-      |                  ).
-      |
-      |
-      | """.stripMargin
+  val path = "/public/theory.txt"
+  val stream: Option[InputStream] = Play.resourceAsStream(path)
+  val text = stream.map(s => Source.fromInputStream(s).mkString)
 
-  val mainTheory = new Theory(mainTheorySource)
+  val mainTheory = text.map(new Theory(_))
 }
